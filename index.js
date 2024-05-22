3292f2691636464a9c1f3b1e8b8793eea357609f
VSRudnyk <v.rudnyk@gmail.com>
2024-05-22 13:57:34 +0300

    change index

--- index.js
+++ index.js
@@ -26,42 +26,46 @@ bot.on('message', async (msg) => {
 
     await bot.sendMessage(
       chatId,
-      'Заходи в наш интернет магазин по кнопке ниже',
+      'Для завантаження зображення перейдіть по кнопці нижче',
       {
         reply_markup: {
           inline_keyboard: [
-            [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }],
+            [
+              {
+                text: 'Перейти до завантаження зображення',
+                web_app: { url: webAppUrl },
+              },
+            ],
           ],
         },
       }
     );
   }
 
-  if (msg?.web_app_data?.data) {
-    try {
-      const data = JSON.parse(msg?.web_app_data?.data);
-      console.log(data);
-      await bot.sendMessage(chatId, 'Спасибо за обратную связь!');
-      await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
-      await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+  //   if (msg?.web_app_data?.data) {
+  //     try {
+  //       const data = JSON.parse(msg?.web_app_data?.data);
+  //       console.log(data);
+  //       await bot.sendMessage(chatId, 'Спасибо за обратную связь!');
+  //       await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+  //       await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
 
-      setTimeout(async () => {
-        await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-      }, 3000);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  //       setTimeout(async () => {
+  //         await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+  //       }, 3000);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   }
 });
 
 app.post('/web-data', async (req, res) => {
   const { responseMessage, queryId } = req.body;
-  console.log(responseMessage);
   try {
     await bot.answerWebAppQuery(queryId, {
       type: 'article',
       id: queryId,
-      title: 'Успешная покупка',
+      title: 'Success',
       input_message_content: {
         message_text: responseMessage,
       },
